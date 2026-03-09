2a0979499c1a26b3a019f7c385c21165c607f649
AJ.k <ali76723201@gmail.com>
2026-03-09 19:31:04 -0400

    fix register with token openai

--- Backend/oauth2.py
+++ Backend/oauth2.py
@@ -5,6 +5,7 @@ oauth2_scheme=OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
 def get_current_user(token:str=Depends(oauth2_scheme)):
+    # print(f"token is: {token}")
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Not Authorized",
--- Backend/routers/meal.py
+++ Backend/routers/meal.py
@@ -12,10 +12,9 @@ router=APIRouter(
 
 @router.post("/")
 def create_meal(request:schemas.MealBase,db:Session=Depends(get_db),current_user:schemas.User=Depends(oauth2.get_current_user)):
+    # print(current_user)
     # Create a new request object with the authenticated user's ID from JWT
-    updated_request = request.model_copy(update={'user_id': current_user.id})
-    return mealRepo.create(updated_request,db,current_user)
-
+    return mealRepo.create(request,db,current_user)
 @router.get("/{id}",response_model=schemas.MealBase)
 def show_meal(id:int,db:Session=Depends(get_db),current_user:schemas.User=Depends(oauth2.get_current_user)):
     return mealRepo.show(id,db)
